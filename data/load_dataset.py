4352b9505d67ee4c091ad465f61c61ee07dbbbba
Michael Sorg <ga27gic@mytum.de>
2019-06-03 10:58:52 +0200

    add data loading with generator

---
+++ data/load_dataset.py
@@ -0,0 +1,125 @@
+import csv
+
+import tensorflow as tf
+import os, shutil, gzip
+
+from tqdm import tqdm
+
+
+def getCategory2IndexDict():
+    d = {'Video DVD':                   0,
+         'Music':                       1,
+         'Books':                       2,
+         'Mobile_Apps':                 3,
+         'Digital_Video_Download':      4,
+         'Digital_Music_Purchase':      5,
+         'Toys':                        6,
+         'Digital_Ebook_Purchase':      7,
+         'PC':                          8,
+         'Camera':                      9,
+         'Wireless':                    10,
+         'Electronics':                 11,
+         'Video':                       12,
+         'Sports':                      13,
+         'Video Games':                 14,
+         'Watches':                     15,
+         'Shoes':                       16,
+         'Home':                        17,
+         'Musical Instruments':         18,
+         'Baby':                        19,
+         'Home Improvement':            20,
+         'Home Entertainment':          21,
+         'Office Products':             22,
+         'Personal_Care_Appliances':    23,
+         'Automotive':                  24,
+         'Lawn and Garden':             25,
+         'Luggage':                     26,
+         'Kitchen':                     27,
+         'Furniture':                   28,
+         'Health & Personal Care':      29,
+         'Software':                    30,
+         'Grocery':                     31,
+         'Pet Products':                32,
+         'Beauty':                      33,
+
+         #UK
+         'Apparel':                     34,
+
+         #US
+         'Tools':                       35,
+         'Outdoors':                    36,
+         'Mobile_Electronics':          37,
+         '2012-12-22':                  38,
+        }
+    return d
+
+
+def downloadData(filename):
+    p = tf.keras.utils.get_file(filename,origin="https://s3.amazonaws.com/amazon-reviews-pds/tsv/" + filename,
+                                extract=False, cache_dir=".", cache_subdir="cache")
+    outDir = p[0:-3]
+    if os.path.exists(outDir) == False:
+        with open(outDir, 'wb') as f_out, gzip.open(p, 'rb') as f_in:
+            shutil.copyfileobj(f_in, f_out)
+    else:
+        print(outDir, "already exists. Skipping unzipping")
+
+    return outDir
+
+
+def csvGenerator(filename, labelindex, inputindex):
+    mapping = getCategory2IndexDict()
+    with open(filename, encoding="utf8") as csv_file:
+        csv.field_size_limit(131072*4)
+        csv_reader = csv.reader(csv_file, delimiter='\t')
+        line_len = len(next(csv_reader))
+        for row in csv_reader:
+            try:
+                yield row[inputindex], mapping[row[labelindex]]
+            except IndexError:
+                print("Index-Error for row:", row)
+
+
+def preprocess(text, label):
+    return text, label
+
+
+def getData(countryCode, batchsize, shuffle, buffer=None, filterOtherLangs=False):
+
+    if countryCode == "TEST":
+        filename = "cache/amazon_reviews_multilingual_TEST_v1_00.tsv"
+    else:
+        filename = "amazon_reviews_multilingual_" + countryCode + "_v1_00.tsv.gz"
+        filename = downloadData(filename)
+
+    dataset = tf.data.Dataset.from_generator(csvGenerator,
+                                             (tf.string,
+                                              tf.int32),
+                                             args=(filename,6,13) )
+
+    if buffer is None:
+        buffer = batchsize * 5
+    if shuffle == True:
+        dataset = dataset.shuffle(buffer)
+
+    #dataset=dataset.map(preprocess)
+
+    dataset = dataset.batch(batchsize).prefetch(tf.data.experimental.AUTOTUNE)
+
+
+    return dataset
+
+
+if __name__== "__main__":
+    bs = 100
+    dataset_train = getData("US", bs, shuffle=False)
+
+    iterator = tf.data.Iterator.from_structure(dataset_train.output_types, dataset_train.output_shapes)
+    train_iterator = iterator.make_initializer(dataset_train)
+
+    with tf.Session() as sess:
+        sess.run(train_iterator)
+
+        for i in tqdm(range(0,1000)):
+            a = sess.run(iterator.get_next())
+            #print(a[0], a[1])
